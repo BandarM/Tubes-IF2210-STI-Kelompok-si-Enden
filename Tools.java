f57d3839ead32435416e07662801cc5fdb0bebb4
BandarM <18214037@std.stei.itb.ac.id>
2016-05-14 22:03:35 +0700

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -1,36 +1,36 @@
-public class Tools extends Inventori{
-	//Atribut
-	private String nama;
-	private boolean statusPakai;
-	
-	//Konstruktor
-	public Tools(String s){
-		super(s);
-		statusPakai = false;
-	}
+public class Tools {
+    //Atribut
+    private String nama;
+    private boolean statusPakai;
 
-	//Getter
-	public String getNama(){
-		return nama;
-	}	
-	public boolean getStatusPakai(){
-		return statusPakai;
-	}
-	
-	//Setter
-	public void setNama(String s){
-		nama = s;
-	}
-	public void setStatusPakai(boolean b) {
-		statusPakai = b;
-	}
-	
-	//Method
-	public void ubahStatusPakai(){
-		if(!statusPakai){
-			statusPakai = true;
-		}else{
-			statusPakai = false;
-		}
-	}
+    //Konstruktor
+    public Tools(String s){
+        nama = s;
+        statusPakai = false;
+    }
+
+    //Getter
+    public String getNama(){
+        return nama;
+    }
+    public boolean getStatusPakai(){
+        return statusPakai;
+    }
+
+    //Setter
+    public void setNama(String s){
+        nama = s;
+    }
+    public void setStatusPakai(boolean b) {
+        statusPakai = b;
+    }
+
+    //Method
+    public void ubahStatusPakai(){
+        if(!statusPakai){
+            statusPakai = true;
+        }else{
+            statusPakai = false;
+        }
+    }
 }
