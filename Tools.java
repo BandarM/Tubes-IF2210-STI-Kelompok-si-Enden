14fee27d0458ea9fb82d3674fc42f2a33a5e976b
kambingtampan <feisalramadhanm@gmail.com>
2016-05-14 11:27:32 +0700

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -1,21 +1,41 @@
-public class Tools{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author feisal
+ */
+public class Tools extends Inventori{
 	private String nama;
-	private String status;
+	private boolean statusPakai;
 	
 	public Tools(String s){
-		this.nama = s;
-		this.status = "Not Equiped";
-		}
+		super(s);
+		statusPakai = false;
+	}
 
 	public void setNama(String s){
-		this.nama = s;
-		}
+		nama = s;
+	}
 	
 	public String getNama(){
-		return this.nama;
-		}
+		return nama;
+	}
 		
-	public String getStatus(){
-		return this.status;
+	public boolean getStatusPakai(){
+		return statusPakai;
+	}
+		
+	public void setStatusPakai(){
+		if(!statusPakai){
+			statusPakai = true;
+		}else{
+			statusPakai = false;
 		}
 	}
+}
+
