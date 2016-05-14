b9014bfcb459dcc75f54a270d7776001c474721d
kambingtampan <feisalramadhanm@gmail.com>
2016-05-14 11:26:56 +0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author feisal
+ */
 import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int i;
+		int i;int neffTas = 0;
 		
-		Pemain utama = new Pemain("Bandar");
+		Pemain utama = new Pemain("Bona");
 		
 		Waktu waktu = new Waktu();
 		Thread t = new Thread(waktu);
@@ -14,9 +25,18 @@ public class Driver {
 		for(i=0; i<10; i++) {
 			utama.cetakPosisi();
 			utama.prtln("Waktu sekarang " + waktu.toString());
-			utama.prt("> ");
 			utama.moveChar(input.next().charAt(0), utama);
-			utama.prt("\n");
-		}
-	}
+                        
+                Inventori[] tasTools = new Inventori[20];
+                tasTools[0] = new Arit ("Arit");
+                tasTools[1] = new Pacul ("Pacul");
+                tasTools[2] = new Penyiram ("Penyiram Air");
+                tasTools[3] = new Biji ("Biji", "Jagung");
+                
+                System.out.print("Isi dalam inventory :");
+                for (Inventori bag: tasTools){
+                    utama.prtln(bag.getNama());
+                }
+                }
+        }
 }
