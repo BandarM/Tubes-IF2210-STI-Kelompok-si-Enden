3422bb7b22db741859be6a0f656ca145cd74421c
BandarM <18214037@std.stei.itb.ac.id>
2016-05-14 14:29:43 +0700

    Update Bangunan.java

--- Bangunan.java
+++ Bangunan.java
@@ -1,39 +1,45 @@
-package javaapplication5;
-
 public class Bangunan{
+	//Atribut
+    private String namaBangunan;
     private Koordinat titikPusat;
     private int panjang;
     private int lebar;
-    private String namaBangunan;
     
-    public Bangunan(String namaBangunan, Koordinat titikPusat, int panjang, int lebar){
-        this.titikPusat = titikPusat;
-        this.namaBangunan = namaBangunan;
+	//Konstruktor
+    public Bangunan(String s, Koordinat k, int p, int l){
+        namaBangunan = s;
+        titikPusat = k;
+        panjang = p;
+        lebar = l;
+    }
+    
+	//Getter
+	public String getNamaBangunan() {
+		return namaBangunan;
+	}
+    public Koordinat getTitikPusat() {
+        return titikPusat;
+    }
+    public int getPanjang() {
+        return panjang;
+    }
+    public int getLebar() {
+        return lebar;
+    }
+	
+	//Setter
+	public void setNamaBangunan(String s) {
+		namaBangunan = s;
+	}
+    public void setTitikPusat(Koordinat k) {
+        titikPusat = k;
+    }
+    public void setPanjang(int panjang) {
         this.panjang = panjang;
+    }
+    public void setLebar(int lebar) {
         this.lebar = lebar;
     }
     
-    public void setPanjang(int panjang){
-        this.panjang=panjang;
-    }
-    
-    public void setLebar(int panjang){
-        this.panjang=panjang;
-    }
-    
-    public void setTitikPusat(int panjang){
-        this.panjang=panjang;
-    }
-    
-    public int getPanjang(){
-        return panjang;
-    }
-    
-    public int getLebar(){
-        return lebar;
-    }
-    
-    public Koordinat getTitikPusat(){
-        return titikPusat;
-    }
+	//Method
 }
