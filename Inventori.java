f044e5c6a7f7b6f03bbe64b6bf1c528c478b5536
BandarM <18214037@std.stei.itb.ac.id>
2016-05-14 21:39:35 +0700

    Update Inventori.java

--- Inventori.java
+++ Inventori.java
@@ -1,24 +1,44 @@
 public class Inventori implements Printable{
-   //Atribut
-   private String nama;
+    //Atribut
+    private static Tools[] alat;
+    private static int neffAlat;
+    int i;
    
-   //Konstruktor
-   public Inventori (String nama){
-        this.nama = nama;
-   }
-   
-   //Getter
-   public String getNama(){
-       return nama;
-   }
-   
-   //Setter
-   public void setNama(String nama){
-       this.nama=nama;
-   }
-   
-   //Method
-   public void cetakInventori(){          //Belum diperlukan
-       System.out.println("Yang ada di dalam Inventory " + nama);
-   }
+    //Konstruktor
+    public Inventori()  {
+        alat = new Tools[6];
+        neffAlat = 0;
+    }
+
+    //Getter
+    public Tools[] getAlat() {
+        return alat;
+    }
+    public Tools getAlat(int i) {
+        return alat[i];
+    }
+    public int neffAlat() {
+        return neffAlat;
+    }
+
+    //Setter
+    public void setTools(Tools[] t) {
+        alat = t;
+    }
+    public void setNeffAlat(int i) {
+        neffAlat = i;
+    }
+
+    //Method
+    public void addTools(Tools t) {
+        alat[neffAlat] = t;
+        neffAlat++;
+    }
+    public void cetakInventori(){
+        prtln("Isi tas >> ");
+        prtln("   <<Tools>>");
+        for(i=0; i<neffAlat; i++) {
+            prtln("     " + alat[i].getNama());
+        }
+    }
 }
