ac05c3b0a52b410db6e253bd73d0e5791503d57a
BandarM <18214037@std.stei.itb.ac.id>
2016-05-14 12:14:22 +0700

    Update Inventori.java

--- Inventori.java
+++ Inventori.java
@@ -1,32 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication5;
-
-/**
- *
- * @author feisal
- */
-//Masih belum, dibuat cuma untuk membantu instansiasi inventori pemain
 public class Inventori implements Printable{
-   
+   //Atribut
    private String nama;
    
+   //Konstruktor
    public Inventori (String nama){
         this.nama = nama;
    }
    
-   public void setNama(String nama){
-       this.nama=nama;
-   }
-   
+   //Getter
    public String getNama(){
        return nama;
    }
    
-   public void cetakInventori(){
+   //Setter
+   public void setNama(String nama){
+       this.nama=nama;
+   }
+   
+   //Method
+   public void cetakInventori(){          //Belum diperlukan
        System.out.println("Yang ada di dalam Inventory " + nama);
    }
 }
