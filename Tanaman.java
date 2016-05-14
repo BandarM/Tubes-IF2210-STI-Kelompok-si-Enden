df3e73e389ac9aad63232d304c82a04f079d30e0
BandarM <18214037@std.stei.itb.ac.id>
2016-05-14 11:25:54 +0700

    Update Tanaman.java

--- Tanaman.java
+++ Tanaman.java
@@ -1,109 +1,120 @@
 public class Tanaman {
+    //Atribut
     private String nama;
     private String jenis;
-    private boolean statussiram;
+    private boolean status;
     private Koordinat posisi;
-    private FaseTumbuhan fase;
-    private int hargaBeli;
-    private int hargaJual;
-    private int faseBibit;
-    private int faseKecambah;
-    private int faseTumbuhan;
-    private int fasePanen;
-    private int waktuHidup;
+    private Fase fase;
+    private int hBeli;
+    private int hJual;
+    private int fBibit;
+    private int fKecambah;
+    private int fTumbuhan;
+    private int fPanen;
+    private int umur;
     
-    public Tanaman(String nama,String jenis, Koordinat posisi){
+    //Konstruktor
+    public Tanaman(String nama,String jenis, Koordinat posisi, int hB, int hJ, int fB, int fK, int fT, int fP){
         this.nama = nama;
         this.jenis = jenis;
         posisi = new Koordinat (posisi);
-        this.fase = FaseTumbuhan.BIBIT;
-        statussiram = false;
-        hargaBeli = 0;
-        hargaJual = 0;
+        this.fase = Fase.BIBIT;
+        status = false;
+        hBeli = hB;
+        hJual = hJ;
+        fBibit = fB;
+        fKecambah = fK;
+        fTumbuhan = fT;
+        fPanen = fP;
+        umur = 0;
     }
-    
-    public void setHargaBeli(int hargaBeli){
-        this.hargaBeli = hargaBeli;
+
+    //Getter
+    public int getHBeli (){
+        return hBeli;
     }
-    
-    public int getHargaBeli (){
-        return hargaBeli;
+    public int getHJual (){
+        return hJual;
     }
-    
-    public void setHargaJual(int hargaJual){
-        this.hargaJual = hargaJual;
-    }
-    
-    public int getHargaJual (){
-        return hargaJual;
-    }
-    
-    public void setPosisi(Koordinat posisi){
-        this.posisi = posisi;
-    }
-    
     public Koordinat getPosisi(){
         return posisi;
     }
-    
-    public void setStatus(boolean status){
-        this.statussiram = status;
-    }
-    
     public boolean getStatus(){
-        return statussiram;
+        return status;
     }
-    
-    public void setNama(String nama){
-        this.nama=nama;
-    }
-    
     public String getNama (){
         return nama;
     }
-    
     public String getJenis(){
         return jenis;
     }
-    
-    public void setFaseBibit(FaseTumbuhan fase){
-        this.fase = FaseTumbuhan.BIBIT;
-    }
-    
-    public void setFaseKecambah(FaseTumbuhan fase){
-        this.fase = FaseTumbuhan.KECAMBAH;
-    }
-    
-    public void setFaseTumbuhan(FaseTumbuhan fase){
-        this.fase = FaseTumbuhan.TUMBUHAN;
-    }
-    
-    public void setFasePanen(FaseTumbuhan fase){
-        this.fase = FaseTumbuhan.PANEN;
-    }
-    
-    public FaseTumbuhan getFase(){
+    public Fase getFase(){
         return fase;
     }
+    public int getUmur() {
+        return umur;
+    }
+
+    //Setter
+    public void setHBeli(int hBeli){
+        this.hBeli = hBeli;
+    }
+    public void setHJual(int hJual){
+        this.hJual = hJual;
+    }
+    public void setPosisi(Koordinat posisi){
+        this.posisi = posisi;
+    }
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public void setFBibit(){
+        this.fase = Fase.BIBIT;
+    }
+    public void setFKecambah(){
+        this.fase = Fase.KECAMBAH;
+    }
+    public void setFTumbuhan(){
+        this.fase = Fase.TUMBUHAN;
+    }
+    public void setFPanen(){
+        this.fase = Fase.PANEN;
+    }
     
+    //Method
     public void ubahFase(){
-        if(fase == FaseTumbuhan.BIBIT){
-            if(waktuHidup==faseBibit){
-                fase=FaseTumbuhan.KECAMBAH;
-                waktuHidup = 0;
+        if(fase == Fase.BIBIT){
+            if(umur==fBibit){
+                fase=Fase.KECAMBAH;
+                umur = 0;
             }
         }
-        if(fase == FaseTumbuhan.KECAMBAH){
-            if(waktuHidup==faseKecambah){
-                fase=FaseTumbuhan.TUMBUHAN;
-                waktuHidup = 0 ;
+        if(fase == Fase.KECAMBAH){
+            if(umur==fKecambah){
+                fase=Fase.TUMBUHAN;
+                umur =0 ;
             }
         }
-        if(fase == FaseTumbuhan.TUMBUHAN){
-            if(waktuHidup==faseTumbuhan){
-                fase=FaseTumbuhan.PANEN;
-                waktuHidup =0;
+        if(fase == Fase.TUMBUHAN){
+            if(umur==fTumbuhan){
+                fase=Fase.PANEN;
+                umur =0;
             }
+        }
+    }
+    public void tambahUmur() {
+        umur++;
+    }
+    public void layu() {
+        int waktuAwal = 0;
+        if(!status) {
+            waktuAwal = umur;
+        }
+        if(umur-waktuAwal == 4) {
+            fase = Fase.LAYU;
         }
     }
 }
