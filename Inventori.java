2b24911806a2c844d26fd534f7859e4266e16516
BandarM <18214037@std.stei.itb.ac.id>
2016-05-16 03:36:24 +0700

    Update Inventori.java

--- Inventori.java
+++ Inventori.java
@@ -1,10 +1,7 @@
-package harvestvalley;
-
 public class Inventori implements Printable {
     //Atribut
     private static Tools[] alat;
     private static int neffAlat;
-    int i;
    
     //Konstruktor
     public Inventori() {
@@ -37,10 +34,25 @@ public class Inventori implements Printable {
         neffAlat++;
     }
     public void cetakInventori() {
+        int i;
+
         prtln("Isi tas >> ");
         prtln("   <<Tools>>");
         for(i=0; i<neffAlat; i++) {
             prtln("     " + alat[i].getNama());
         }
+    }
+    
+    //@Override
+    public String toString() {
+        int i;
+        String[] temp = new String[30];
+        temp[29] = new String("  Isi tas >>\n   <<Tools>>\n");
+        for(i=0; i<neffAlat; i++) {
+            temp[i] = new String("       " + alat[i].getNama() + "\n");
+            temp[29] += temp[i];
+        }
+        
+        return (temp[29]);
     }
 }
