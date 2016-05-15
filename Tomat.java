3596def9e932f9a345ac620da4731bdc42a3ec61
BandarM <18214037@std.stei.itb.ac.id>
2016-05-15 23:20:57 +0700

    Update Tomat.java

--- Tomat.java
+++ Tomat.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bonapelir;
-
-/**
- *
- * @author Enden Lukman
- */
 public class Tomat extends Tanaman {
     public Tomat(Koordinat posisi){
         super("Tomat",false,posisi,30,10,1,2,3,1);
