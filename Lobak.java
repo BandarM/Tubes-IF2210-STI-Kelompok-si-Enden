d4a4d67d75ec2b92796bb711bfa68e0e49f07dfc
BandarM <18214037@std.stei.itb.ac.id>
2016-05-15 23:17:03 +0700

    Update Lobak.java

--- Lobak.java
+++ Lobak.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bonapelir;
-
-/**
- *
- * @author Enden Lukman
- */
 public class Lobak extends Tanaman {
     public Lobak(Koordinat posisi){
         super("Lobak",false,posisi,40,12,1,2,3,2);
