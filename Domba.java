fda10a41ee2543268e0be0faf3faa39978db3cda
KehedSia <enden.luqman@gmail.com>
2016-05-16 21:16:29 +0700

    Update Domba.java

--- Domba.java
+++ Domba.java
@@ -1,55 +1,5 @@
 public class Domba extends Hewan {
-    //Atribut
-    private String nama;
-    private String jenis;
-    private boolean status;
-    private Koordinat lokasi;
-
-    //Konstruktor
-    public Domba(String nama, boolean status, Koordinat lokasi, String jenis) {
-        super(nama, status, lokasi, jenis);
-        this.nama = nama;
-        this.jenis = jenis;
-        this.lokasi = lokasi;
-        this.status = status;
+    public Domba(String nama, String jenis, Koordinat lokasi){
+		super(nama,jenis,lokasi,50,100,2,4,3);
     }
-    
-    //Getter
-    public String getNama() {
-        return nama;
-    }
-    @Override
-    public String getFase() {
-        if (umur <= 3) {
-            fase = "Kecil";
-        }
-        else if (umur > 8) {
-            fase = "Besar";
-            if (umur % 2 == 0) {
-                fase = "Besar (Panen)";
-            }
-        }
-        return fase;
-    }
-    @Override
-    public String getJenis() {
-        return this.jenis = "Domba";
-    }
-    @Override
-    public int getHargaBeli() {
-        if ("Kecil".equals(fase)) {
-            hargabeli = 50;
-        } else {
-            hargabeli = 70;
-        }
-        return hargabeli;
-    }
-    @Override
-    public int getHargaJual() {
-        return hargajual = 100;
-    }
-    
-    //Setter
-    
-    //Method
 }
