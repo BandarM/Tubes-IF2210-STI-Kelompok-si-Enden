4f7096d1476793dbe34efe69c6120c8dac9d8e1d
kambingtampan <feisalramadhanm@gmail.com>
2016-05-16 20:45:38 +0700

    Update Moveable.java

--- Moveable.java
+++ Moveable.java
@@ -1,21 +1,31 @@
 public interface Moveable { //Menggunakan generic rencananya
-	default void moveChar(char input, Pemain p) {
+	default void moveChar(char input, Pemain p, ArrayKoordinatTerpakai a) {
 		Koordinat pos = p.getPosisi();
 
 		if(input == 'w') {
 			if(pos.getArah() == Orientasi.ATAS) {
 				if(pos.getY() != 0) {
-					pos.setY(pos.getY() - 1);
+					for(int i=0; i<a.getNeff(); i ++) {
+						Koordinat temp = a.getUnavailable(i);
+						if((pos.getY()!=temp.getY())&&(pos.getX() != temp.getX())){
+							pos.setY(pos.getY() - 1);
+						}
+					}
 				} else {
 					//Throw outofbound exception
-				}
+			}
 			} else {
 				pos.setArah(Orientasi.ATAS);
 			}
 		} else if(input == 'd') {
 			if(pos.getArah() == Orientasi.KANAN) {
 				if(pos.getX() != 30) {
-					pos.setX(pos.getX() + 1);
+					for(int i=0; i<a.getNeff(); i ++) {
+						Koordinat temp = a.getUnavailable(i);
+						if((pos.getY()!=temp.getY())&&(pos.getX() != temp.getX())){
+							pos.setX(pos.getX() + 1);
+						}
+					}
 				} else {
 					//Throw outofbound exception
 				}
@@ -25,7 +35,12 @@ public interface Moveable { //Menggunakan generic rencananya
 		} else if(input == 's') {
 			if(pos.getArah() == Orientasi.BAWAH) {
 				if(pos.getY() != 31) {
-					pos.setY(pos.getY() + 1);
+					for(int i=0; i<a.getNeff(); i ++) {
+						Koordinat temp = a.getUnavailable(i);
+						if((pos.getY()!=temp.getY())&&(pos.getX() != temp.getX())){
+							pos.setY(pos.getY() + 1);
+						}
+					}
 				} else {
 					//Throw outofbound exception
 				}
@@ -35,7 +50,12 @@ public interface Moveable { //Menggunakan generic rencananya
 		} else if(input == 'a') {
 			if(pos.getArah() == Orientasi.KIRI) {
 				if(pos.getX() != 0) {
-					pos.setX(pos.getX() - 1);
+					for(int i=0; i<a.getNeff(); i ++) {
+						Koordinat temp = a.getUnavailable(i);
+						if((pos.getY()!=temp.getY())&&(pos.getX() != temp.getX())){
+							pos.setX(pos.getX() - 1);
+						}
+					}
 				} else {
 					//Throw outofbound exception
 				}
